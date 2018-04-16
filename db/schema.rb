6eb7a1b9a253c60cd3fa292348380319ab8e04c9
盧　家棟 <>
2018-04-16 14:00:40 +0900

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415160458) do
+ActiveRecord::Schema.define(version: 20180416030001) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180415160458) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "rest_digest"
+    t.datetime "rest_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
